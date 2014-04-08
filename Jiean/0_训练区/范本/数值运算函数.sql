df2726742bcf45b06ce341bfd1eac315d4216968
lwqhp <lwqhp@aliyun.com>
2014-04-08 10:18:32 +0800

    up

--- Jiean/0_训练区/范本/数值运算函数.sql
+++
@@ -1,7 +0,0 @@
-
---数值运算函数
-select abs(-1234123)
-
-select round(234.3245,2)
-
-select ceiling(234.3245)
--- Jiean/应用-基础语句/存储过程格式/收式.sql
+++
@@ -1,8 +0,0 @@
-
-/*
-收式
-*/
-
---清除临时表
-	If OBJECT_ID('tempdb.dbo.#StateList') Is Not Null 
-		Drop Table #StateList		
